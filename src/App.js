9a0f9c47418d9e0c2d998079d4cf022f912692eb
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-03-20 13:44:29 -0300

    implement CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Banner from './componentes/Banner/Banner.js';
+import CampoTexto from './componentes/CampoTexto/CampoTexto.js';
 
 function App() {
   return (
     <div className="App">
-            <Banner />
+          <Banner />
+          <CampoTexto label='Nome' placeholder='Digite seu nome'/>
+          <CampoTexto label='Cargo'placeholder='Digite seu cargo'/>
+          <CampoTexto label='Imagem'placeholder='Digite o endereço da imagem'/>
     </div>
   );
 }
