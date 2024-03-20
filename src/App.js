13329fb22668e5d11e7c6151bde2530b1fb364a0
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-03-20 13:56:33 -0300

    implement Formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Banner from './componentes/Banner/Banner.js';
 import CampoTexto from './componentes/CampoTexto/CampoTexto.js';
+import Formulario from './componentes/Formulario/Formulario.js';
 
 function App() {
   return (
     <div className="App">
           <Banner />
-          <CampoTexto label='Nome' placeholder='Digite seu nome'/>
-          <CampoTexto label='Cargo'placeholder='Digite seu cargo'/>
-          <CampoTexto label='Imagem'placeholder='Digite o endereço da imagem'/>
+          <Formulario />
     </div>
   );
 }
